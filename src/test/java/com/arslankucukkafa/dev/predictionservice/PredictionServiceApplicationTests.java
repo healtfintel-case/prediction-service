01aaac7c057a19f7e8854f80ac553e525c8e092f
Arslan Kucukkafa <arslankucukkafa@gmail.com>
2025-06-01 12:59:19 +0300

    initial commit

---
+++ src/test/java/com/arslankucukkafa/dev/predictionservice/PredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arslankucukkafa.dev.predictionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
